b3a08101f26511ed79e734836683126b9f3096ef
Chris Lee <chlee@vassar.edu>
2017-12-11 20:03:26 -0500

    finished dayOne

--- WeightLiftingApp/DayOne.java
+++ WeightLiftingApp/DayOne.java
@@ -1,11 +1,36 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 
 public class DayOne extends Workout
 {
     private JFrame screen;
+    private JLabel titleDay;
+    private JLabel titleWeek;
+    private JLabel lowerBodyLabel;
+    private JLabel lowerBodyLift;
+    private JLabel upperBody1Label;
+    private JLabel upperBody1Lift;
+    private JLabel subset1Label;
+    private JLabel subset1Lift;
+    private JLabel upperBody2Label;
+    private JLabel upperBody2Lift;
+    private JLabel subset2Label;
+    private JLabel subset2Lift;
+    private JLabel core1Label;
+    private JLabel core1Lift;
+    private JLabel core2Label;
+    private JLabel core2Lift;
+    private JButton finish;
+    
+    private Border borderLB1;
+    
+    
+    Button b = new Button();
+    
+    
     public DayOne(int week)
     {
         super(week);        
@@ -14,17 +39,124 @@ public class DayOne extends Workout
     public void display()
     {
         screen = new JFrame();
-        int maxFS;
-        screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        int xStart[]={200,400,400,200};
-        int yStart[]={450,450,550,550};
+        
+        screen.setSize(500,500);
+        screen.setDefaultCloseOperation(screen.EXIT_ON_CLOSE);
+        screen.setLayout(new GridLayout(9,2,10,10));
+        
+        titleDay = new JLabel("Day One Workout", SwingConstants.CENTER);
+        titleWeek = new JLabel("Week One Workout", SwingConstants.CENTER);
+        lowerBodyLabel = new JLabel("<html>FrontSquat<br>"+ 
+                                       primaryLowerBodyLabel() +"</html>",
+                                       SwingConstants.CENTER);
+        lowerBodyLift = new JLabel("<html>"+primaryLowerBodyLift1()+"<br>"+ 
+                                       primaryLowerBodyLift2()+"</html>"
+                                       ,SwingConstants.CENTER);
+        upperBody1Label = new JLabel("DB Chest Press", SwingConstants.CENTER);
+        upperBody1Lift = new JLabel("4 x 6", SwingConstants.CENTER);
+        subset1Label = new JLabel("SB Leg Curls", SwingConstants.CENTER);
+        subset1Lift = new JLabel("3 x 10",SwingConstants.CENTER);
+        upperBody2Label = new JLabel("Weighted Pull-ups", SwingConstants.CENTER);
+        upperBody2Lift = new JLabel("4 x 5", SwingConstants.CENTER);
+        subset2Label = new JLabel("Chest Supported T's", SwingConstants.CENTER);
+        subset2Lift = new JLabel("3 x 10", SwingConstants.CENTER);
+        core1Label = new JLabel("Deadbugs", SwingConstants.CENTER);
+        core1Lift = new JLabel("2 x 10ea",SwingConstants.CENTER);
+        core2Label = new JLabel("Anti-Band Rotation", SwingConstants.CENTER);
+        core2Lift = new JLabel("2 x 10ea", SwingConstants.CENTER);
+        finish = new JButton("End Workout");
+        
+        finish.addActionListener(b);
+        
+        screen.add(titleDay);
+        screen.add(titleWeek);
+        screen.add(lowerBodyLabel);
+        screen.add(lowerBodyLift);
+        screen.add(upperBody1Label);
+        screen.add(upperBody1Lift);
+        screen.add(subset1Label);
+        screen.add(subset1Lift);
+        screen.add(upperBody2Label);
+        screen.add(upperBody2Lift);
+        screen.add(subset2Label);
+        screen.add(subset2Lift);
+        screen.add(core1Label);
+        screen.add(core1Lift);
+        screen.add(core2Label);
+        screen.add(core2Lift);
+        screen.add(finish);
+        
         screen.setVisible(true);
-        // Not sure if this will go line by line yet
-        JLabel label = new JLabel("Enter your max front squat weight: ");
-        screen.add(label);
-        label.setText("Here is your workout");
-        // Change workout and place exercises here
-        label.setText("4A. DEADBUGS: 2x10ea");
-        label.setText("4B. BAND ANTIROTATION: 2x10ea");
+    }
+    
+    
+    private class Button implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            if (e.getSource()==finish)
+            {
+                System.out.println("works");
+            }
+        }
+    }
+    
+    
+    private String primaryLowerBodyLabel()
+    {
+        String s = "";
+        if (week>4 && week <=8)
+        {
+            s="(+10 lbs)";
+        }
+        else if (week>8 && week <=12)
+        {
+            s="(+20 lbs)";
+        }
+        return s;
+    }
+    
+    private String primaryLowerBodyLift1()
+    {
+        String s = "";
+        if (week%4==1)
+        {
+            s = "5,5,5,5,5+";
+        }
+        else if (week%4==2)
+        {
+            s = "5,5,5,3,3+";
+        }
+        else if (week%4==3)
+        {
+            s = "5,5,5,3,1+";
+        }
+        else if (week%4==0)
+        {
+            s = "5,5,5";
+        }
+        return s;
+    }
+    
+    private String primaryLowerBodyLift2()
+    {
+        String s = "";
+        if (week%4==1)
+        {
+            s = "50,60,70,75,80";
+        }
+        else if (week%4==2)
+        {
+            s = "50,60,70,80,85";
+        }
+        else if (week%4==3)
+        {
+            s = "50,60,75,85,90";
+        }
+        else if (week%4==0)
+        {
+            s = "50,60,70";
+        }
+        return s;
     }
 }
--- WeightLiftingApp/startWorkoutUI.java
+++ WeightLiftingApp/startWorkoutUI.java
@@ -9,7 +9,6 @@ public class StartWorkoutUI extends BaseUI
     private JLabel weekL;
     private JTextField dayTF;
     private JTextField weekTF;
-    private JButton enter;
     private JButton start;
     private JLabel error;
     private int day;
@@ -32,24 +31,21 @@ public class StartWorkoutUI extends BaseUI
 
         screen.setSize(500,500);
         screen.setDefaultCloseOperation(screen.EXIT_ON_CLOSE);
-        screen.setLayout(new GridLayout(7,1,10,10));
+        screen.setLayout(new GridLayout(6,1,10,10));
 
         dayL = new JLabel("What day are you on?",JLabel.CENTER);
         weekL = new JLabel("What week are you on?",JLabel.CENTER);
         dayTF = new JTextField(10);
         weekTF = new JTextField(10);
-        enter = new JButton("Enter");
         start = new JButton("Start Workout");
         error = new JLabel();
 
-        enter.addActionListener(b);
         start.addActionListener(b);
 
         screen.add(dayL);
         screen.add(dayTF);
         screen.add(weekL);
         screen.add(weekTF);
-        screen.add(enter);
         screen.add(start);
         screen.add(error);
 
@@ -60,8 +56,10 @@ public class StartWorkoutUI extends BaseUI
     {
         public void actionPerformed(ActionEvent e)
         {
-            if (e.getSource()==enter)
+            if (e.getSource()==start)
             {
+                day=0;
+                week=0;
                 dayIsNum=sm.isStringANum(dayTF.getText());
                 weekIsNum=sm.isStringANum(weekTF.getText());
 
@@ -74,11 +72,9 @@ public class StartWorkoutUI extends BaseUI
                 {
                     error.setText("Please enter a number for day and week");
                 }
-            }
-            if (e.getSource()==start)
-            {
                 BuildProgram lift = new BuildProgram();
                 lift.createWorkout(day,week);
+                screen.setVisible(false);
             }
         }
     }
